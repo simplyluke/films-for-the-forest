95d879938708a69fdf95e00738617d0647ca39bc
Luke Wright <lukewright1997@gmail.com>
2013-08-08 14:19:36 -0500

    remove gantry logo

--- templates/gantry/features/branding.php
+++ templates/gantry/features/branding.php
@@ -8,19 +8,21 @@
 * Gantry uses the Joomla Framework (http://www.joomla.org), a GNU/GPLv2 content management system
 *
 */
+
 defined('JPATH_BASE') or die();
 
 gantry_import('core.gantryfeature');
-
 class GantryFeatureBranding extends GantryFeature {
     var $_feature_name = 'branding';
 
 	function render($position) {
 	    ob_start();
 	    ?>
+	    <!--
 	    <div class="rt-block">
 			<a href="http://www.gantry-framework.org/" title="Gantry Framework" class="powered-by"></a>
 		</div>
+	-->
 		<?php
 	    return ob_get_clean();
 	}
